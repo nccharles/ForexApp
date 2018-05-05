91c364a1cd9f13caad2c3d08185e77bfa1c7fc01
totti1 <stotestorch@gmail.com>
2018-05-05 16:22:37 +0200

    charles

--- app/Screens/Map.js
+++ app/Screens/Map.js
@@ -5,80 +5,117 @@ import MapViewDirections from 'react-native-maps-directions'
 import {
   Platform, Text, View, StyleSheet, Dimensions
 } from 'react-native';
-import styles from './Style/MapStyle';
-// import { RunInfo, RunInfoNumeric } from '../components/footer';
-
-
+import styles from '../shared-styles';
+import { RunInfo, RunInfoNumeric } from '../components/footer';
 let { width, height } = Dimensions.get('window')
 const ASPECT_RATIO = width / height
-const LATITUDE_DELTA = 0.00772
+const LATITUDE_DELTA = 0.07003
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO
-
 const locOptions = {
   enableHighAccuracy: true,
   distanceInterval: 3
 }
-const APIKEY= 'AIzaSyB2L8kbuQG1j0XAeDuYoHm_Ql-8fwRGSms';
+const APIKEY = 'AIzaSyB2L8kbuQG1j0XAeDuYoHm_Ql-8fwRGSms';
 class MapScreen extends Component {
-
   state = {
     coords: {},
+    selectedMarker: ''
   }
-
   logData = async () => {
     let { status } = await Permissions.askAsync(Permissions.LOCATION);
     if (status === 'granted') {
-      const APIKEY= await Location.setApiKey('AIzaSyB2L8kbuQG1j0XAeDuYoHm_Ql-8fwRGSms');
+      const APIKEY = await Location.setApiKey('AIzaSyB2L8kbuQG1j0XAeDuYoHm_Ql-8fwRGSms');
       await Location.setApiKey('AIzaSyB2L8kbuQG1j0XAeDuYoHm_Ql-8fwRGSms');
       await Location.watchPositionAsync(
         locOptions,
         (coords) => this.setState(coords)
       );
-    }else{
+    } else {
       return alert('Enable to Access your location');
     }
   }
-
   componentWillUnmount() {
     Location.watchHeadingAsync().remove()
   }
-
   renderUserLocation = () => {
-
     const { longitude, latitude } = this.state.coords
-    const coordinates=[
-      {
-        latitude: -1.9793953,
-        longitude: 30.1053328,
-      },
+    const coordinates = [
       {
         latitude: latitude,
         longitude: longitude,
+      },
+      {
+        latitude: -1.9349459,
+        longitude: 30.0937492,
+      },
+      {
+        latitude: -2.0373686,
+        longitude: 30.3132083,
+      },
+      {
+        latitude: -1.9444369,
+        longitude: 30.0529602,
+      },
+      {
+        latitude: -1.953317,
+        longitude: 30.1007112,
+      },
+      {
+        latitude: -1.949344,
+        longitude: 30.060989,
+      },
+      {
+        latitude: -1.945372,
+        longitude: 30.125211,
+      },
+      {
+        latitude: -1.943378,
+        longitude: 30.059138,
+      },
+      {
+        latitude: -1.941366,
+        longitude: 30.128402,
       }
-
+      ,
+      {
+        latitude: -1.943371,
+        longitude: 30.058922,
+      },
+      {
+        latitude: -1.944948,
+        longitude: 30.061040,
+      }
     ]
+   
     return (
       <View>
-      <MapView.Marker coordinate={coordinates[0]} title={"Isange Group Ltd, Kigali"}/>
-      <MapView.Marker coordinate={coordinates[1]} title={"Your location"}/>
-      <MapViewDirections 
-      origin={coordinates[1]}
-      destination={coordinates[0]}
-      strokeWidth= {3}
-      apikey={APIKEY}
-      strokeColor="#006442"
-      />
-     </View>
+        <MapView.Marker coordinate={coordinates[0]} title={"Your location"} />
+        <MapView.Marker coordinate={coordinates[1]} onPress={() => {this.setState({selectedMarker:coordinates[1]})}} title={"Bravia Forex Bureau"} />
+        <MapView.Marker coordinate={coordinates[2]} onPress={() => {this.setState({selectedMarker:coordinates[2]})}} title={"Diamonds Forex Bureau"} />
+        <MapView.Marker coordinate={coordinates[3]} onPress={() => {this.setState({selectedMarker:coordinates[3]})}} title={"RG Forex Bureau"} />
+        <MapView.Marker  coordinate={coordinates[4]} onPress={() => {this.setState({selectedMarker:coordinates[4]})}} title={"Prime Forex Bureau"}/>
+        <MapView.Marker coordinate={coordinates[5]} onPress={() => {this.setState({selectedMarker:coordinates[5]})}} title={"La Fortune Forex Bureau"} />
+        <MapView.Marker coordinate={coordinates[6]} onPress={() => {this.setState({selectedMarker:coordinates[6]})}} title={"Welcome Forex Bureau"} />
+        <MapView.Marker coordinate={coordinates[7]} onPress={() => {this.setState({selectedMarker:coordinates[7]})}} title={"Muhabura Forex Bureau"} />
+        <MapView.Marker coordinate={coordinates[8]} onPress={() => {this.setState({selectedMarker:coordinates[8]})}} title={"Rebex Forex Bureau"} />
+        <MapView.Marker coordinate={coordinates[9]} onPress={() => {this.setState({selectedMarker:coordinates[9]})}} title={"Shahanshah Forex Bureau Ltd"} />
+        <MapView.Marker coordinate={coordinates[10]} onPress={() => {this.setState({selectedMarker:coordinates[10]})}} title={"Dahabshiil"} />
+        <MapViewDirections
+          origin={coordinates[0]}
+          destination={this.state.selectedMarker}
+          strokeWidth={5}
+          apikey={APIKEY}
+          strokeColor="#006442"
+        />
+      </View>
     );
   }
-
-
   render() {
     this.logData();
-    const { longitude, latitude,speed } = this.state.coords
+    console.log(this.state.selectedMarker)
+    const { longitude, latitude, speed } = this.state.coords
     return (
       <View style={styles.container} >
-
         {
           latitude &&
           <MapView
@@ -92,9 +129,8 @@ class MapScreen extends Component {
           >
             {this.renderUserLocation()}
           </MapView>
-
         }
-        {/* <View style={styles.infoWrapper}>
+        <View style={styles.infoWrapper}>
           <RunInfoNumeric title='Distance' unit='Km'
             ref={(info) => this.state.distanceInfo = info}
           />
@@ -104,11 +140,9 @@ class MapScreen extends Component {
           <RunInfo title='Direction' value='NE'
             ref={(info) => this.directionInfo = info}
           />
-        </View> */}
+        </View>
       </View>
-
     );
   }
 }
-
-export default MapScreen;
+export default MapScreen
